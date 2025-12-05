64c153fb7c676cd12ba9cfb9a3dba45108b75ba1
Aanshik Sharma <aanshik16@gmail.com>
2025-12-05 12:32:39 +0530

    Very minor ui fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
           setTechFilter={setTechFilter}
         />
 
-        <section className="max-w-5xl mx-auto py-6 space-y-6 bg-neutral-50 rounded-xl shadow">
+        <section className="max-w-5xl mx-auto py-6 mb-6 space-y-6 bg-neutral-50 rounded-xl shadow">
           <h2 className="relative text-xl text-neutral-900 font-semibold px-4 sm:px-6">
             <span className="absolute top-0 left-0 bg-accent w-1 rounded-r-full h-full"></span>
             Developers
